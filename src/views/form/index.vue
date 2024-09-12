cc6fe3d72a514a9794077cbeda7868cb976d11fb
weizwz <1124725517@qq.com>
2024-09-12 17:43:02 +0800

    feat: 更新表单基础页

--- src/views/form/index.vue
+++ src/views/form/index.vue
@@ -239,6 +239,18 @@ const handleReset = () => {
 
 <template>
   <el-card>
+    <template #header>
+      <div class="font-medium">
+        基础表单，配置项都是固定数据，例如下拉、单选多选、日期时间等
+      </div>
+      <el-link
+        class="mt-2"
+        href="https://github.com/weizwz/weiz-vue3-ts/blob/main/src/views/form/index.vue"
+        target="_blank"
+      >
+        代码位置 src/views/form/index.vue
+      </el-link>
+    </template>
     <PlusForm
       v-model="state"
       label-position="right"
